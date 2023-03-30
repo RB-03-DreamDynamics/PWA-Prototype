9e9fa9ad48481dd398cbc7dae2778f20605008da
Jules Houben <jules.houben@live.nl>
2023-03-30 14:18:11 +0200

    Add fonts and background gradient

--- src/views/login.vue
+++ src/views/login.vue
@@ -3,22 +3,24 @@
 </script>
 
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <h1>Login</h1>
-            </div>  
-        </div>
-
-        <div class="row">
-            <div class="col-12">
-                <Login username="" password="" />
+    <div class="bg-login">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h1>Login</h1>
+                </div>  
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-12">
-                <a href="/login/qr">Login with QR code</a>
+            <div class="row">
+                <div class="col-12">
+                    <Login username="" password="" />
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <a href="/login/qr">Login with QR code</a>
+                </div>
             </div>
         </div>
     </div>
