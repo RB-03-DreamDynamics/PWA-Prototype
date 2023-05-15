ad233ef7fab4e5c58e24abeb1983fa5d4269f2ed
Jules Houben <jules.houben@live.nl>
2023-05-15 11:27:14 +0200

    Add boundaries for numeric inputs

--- src/components/Forms/FormRenderer.vue
+++ src/components/Forms/FormRenderer.vue
@@ -5,7 +5,12 @@
         <h1>{{ form.title }}</h1>
         <p>Description: {{ form.description }}</p>
         <div v-for="element in form.design.elements as FormElement[]" :key="element.element_id">
-          <component :is="fieldComponent(element.field?.type)" v-if="element.element_type === 'field'" :field="element.field" :label="element.text"></component>
+           <component 
+            :is="fieldComponent(element.field?.type)" 
+            v-if="element.element_type === 'field'"
+            v-bind="fieldProps(element)"
+            @input="handleInput"
+          ></component>
         </div>
       </div>
     </div>
@@ -18,6 +23,16 @@ import TextField from "./form-fields/TextField.vue";
 import NumericField from "./form-fields/NumericField.vue";
 import DateField from "./form-fields/DateField.vue";
 // Import other field components as needed
+
+interface FormElement {
+  element_id: number;
+  element_type: string;
+  text: string;
+  field?: {
+    field_id: number;
+    type: string;
+  };
+}
 
 export default defineComponent({
   name: "FormRenderer",
@@ -53,20 +68,46 @@ setup(props) {
     }
   };
 
+  const fieldProps = (element) => {
+    switch (element.field?.type) {
+      case "text":
+        return {
+          value: element.field.default_value,
+          label: element.text,
+          elementId: 'textField-' + element.element_id,
+          required: element.field.required,
+          readOnly: element.field.read_only,
+        };
+      case "numeric":
+        return {
+          value: element.field.default_value,
+          minValue: element.field.min_numeric_value,
+          maxValue: element.field.max_numeric_value,
+          step: element.field.only_integers ? 1 : 0.1,
+          required: element.field.required,
+          readOnly: element.field.read_only,
+          label: element.text,
+          elementId: 'numericField-' + element.element_id,
+        };
+      case "date":
+        return {
+          value: element.field.default_value,
+          required: element.field.required,
+          readOnly: element.field.read_only,
+          label: element.text,
+          elementId: 'dateField-' + element.element_id,
+        };
+      default:
+        return {};
+      }
+    };
+
   return {
-    fieldComponent
+    fieldComponent,
+    fieldProps
   };
 },
 
 });
-interface FormElement {
-  element_id: number;
-  element_type: string;
-  text: string;
-  field?: {
-    field_id: number;
-    type: string;
-  };
-}
 
 </script>
--- src/components/Forms/form-fields/NumericField.vue
+++ src/components/Forms/form-fields/NumericField.vue
@@ -1,15 +1,61 @@
 <template>
-    <input type="number" :value="value" @input="$emit('input', $event.target.value)" />
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      value: {
-        type: Number,
-        default: 0,
-      },
+  <div class="mb-3">
+    <label :for="elementId" class="form-label">{{ label }}</label>
+    <input 
+      type="number" 
+      :id="elementId"
+      :value="value" 
+      :min="minValue"
+      :max="maxValue"
+      :step="step"
+      :required="required"
+      :readonly="readOnly"
+      @input="onInput" 
+      class="form-control"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0,
     },
-  };
-  </script>
-  
+    minValue: {
+      type: Number,
+      default: null,
+    },
+    maxValue: {
+      type: Number,
+      default: null,
+    },
+    step: {
+      type: Number,
+      default: 1,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+    readOnly: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    elementId: {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    onInput(event) {
+      this.$emit('input', +event.target.value);
+    },
+  },
+};
+</script>
