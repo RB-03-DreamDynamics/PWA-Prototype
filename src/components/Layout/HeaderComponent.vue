bcd155feed452bee11509a40c22e8ae6ab4e366f
Jules Houben <jules.houben@live.nl>
2023-05-15 14:10:45 +0200

    Add logo to headercomponent

--- src/components/Layout/HeaderComponent.vue
+++ src/components/Layout/HeaderComponent.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="container header">
-        <div class="row">
-
+        <div class="row d-flex justify-content-center mt-2">
+            <div class="col-auto">
+                <img src="@/assets/images/zenya-capture-logo.png" alt="Zenya Capture logo" class="logo-header">
+            </div>
+            <div class="col-auto d-flex">
+                <p class="mb-0 me-1 fw-bold logo-text fs-2">Zenya</p>
+                <p class="mb-0 fw-bold zenya-color logo-text fs-2">Capture</p>
+            </div>
         </div>
-        <div class="row text-center mt-1">
+        <div class="row text-center">
             <div class="status-indicator">
                 <span :class="{ 'online': isOnline, 'offline': !isOnline }"></span>
                 <p v-if="isOnline">Online</p>
                 <p v-else>Offline</p>
             </div>
-            
         </div>
     </div>
 </template>
