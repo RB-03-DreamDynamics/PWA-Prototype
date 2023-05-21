6c1c0e257b2d7f69828c6f11946963b511a8cc6e
Jules Houben <jules.houben@live.nl>
2023-05-21 15:11:44 +0200

    update footer with icons and updated layout

--- src/components/Layout/FooterComponent.vue
+++ src/components/Layout/FooterComponent.vue
@@ -2,6 +2,7 @@
 import { defineComponent } from 'vue';
 
 
+
 export default defineComponent({
     name: 'FooterComponent',
     setup() {
@@ -16,30 +17,43 @@ export default defineComponent({
     <div class="container footer">
         <div class="row">
             <div class="col-2">
-                <div class="row"></div>
                 <div class="row">
-                    <p>Instellingen</p>
+                    <img src="@/assets/icons/mdi-settings.svg" alt="Settings Icon" class="footer-icons">
+                </div>
+                <div class="row">
+                    <p class="p-0 m-0">Instellingen</p>
                 </div>
             </div>
             <div class="col-2">
                 <div class="row">
-
+                    <img src="@/assets/icons/mdi-empty-message.svg" alt="Empty Messages Icon" class="footer-icons">
                 </div>
                 <div class="row">
-                    <p>Mijn meldingen</p>
-                </div>
-            </div>
-            <div class="col"></div>
-            <div class="col-2">
-                <div class="row"></div>
-                <div class="row">
-                    <p>Info & Feedback</p>
+                    <p class="p-0 m-0 lh-1">Mijn meldingen</p>
                 </div>
             </div>
+            <div class="col d-flex justify-content-center">
+                <router-link to="/dashboard">
+                    <div class="footer-add-button">
+                        <p class="fs-1">+</p>
+                    </div>
+                </router-link>
+                
+            </div>
             <div class="col-2">
-                <div class="row"></div>
                 <div class="row">
-                    <p>Uitloggen</p>
+                    <img src="@/assets/icons/mdi-info.svg" alt="Info Icon" class="footer-icons">
+                </div>
+                <div class="row">
+                    <p class="p-0 m-0 lh-1">Info & Feedback</p>
+                </div>
+            </div>
+            <div class="col-2">
+                <div class="row">
+                    <img src="@/assets/icons/mdi-logout.svg" alt="Logout icon" class="footer-icons">
+                </div>
+                <div class="row">
+                    <p class="p-0 m-0">Uitloggen</p>
                 </div>
             </div>
         </div>
